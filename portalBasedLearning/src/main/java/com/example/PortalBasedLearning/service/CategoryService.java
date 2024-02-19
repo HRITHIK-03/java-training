9a56781a2d6a83a0dd86cf8d3b48a1ab2d3a1bf3
HRITHIK-03 <71772495+HRITHIK-03@users.noreply.github.com>
2024-02-19 23:41:17 +0530

    Update CategoryService.java

--- portalBasedLearning/src/main/java/com/example/PortalBasedLearning/service/CategoryService.java
+++ portalBasedLearning/src/main/java/com/example/PortalBasedLearning/service/CategoryService.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service;
 
 import com.example.portalBasedLearning.entity.CategoryEntity;
 import com.example.portalBasedLearning.repository.CategoryRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
