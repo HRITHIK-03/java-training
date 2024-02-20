e6f5212b681cad40de968f2a56f53d0420547b67
HRITHIK-03 <71772495+HRITHIK-03@users.noreply.github.com>
2024-02-20 15:22:40 +0530

    Update native query in Courserepoisitory.java

--- portalBasedLearning/src/main/java/com/example/PortalBasedLearning/repository/Courserepoisitory.java
+++ portalBasedLearning/src/main/java/com/example/PortalBasedLearning/repository/Courserepoisitory.java
@@ -7,5 +7,7 @@ import com.example.portalBasedLearning.entity.CourseEntity;
 
 @Repository
 public interface Courserepoisitory extends JpaRepository<CourseEntity, Long>{
+  @Query(value = "SELECT * from Course c WHERE c.title = ?1", nativeQuery = true)
+	List<Course> findAllJavaCourses(String name);
 
 }
